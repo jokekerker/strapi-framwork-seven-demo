ea2d763886d127e1ed34cc9cdadc7e8d9c128d53
jokekerker <jokekerker@gmail.com>
2023-10-05 16:01:05 +1000

    [UPDATE] change instance type to lower spec

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    instance_types = ["c6g.xlarge"]
 
     attach_cluster_primary_security_group = true
   }
